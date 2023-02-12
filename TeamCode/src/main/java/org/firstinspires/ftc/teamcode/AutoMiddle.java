c1ef598b657f149070b25e0e8744f50d8930de60
szjstar <73240804+szjstar@users.noreply.github.com>
2023-02-12 17:22:20 +0800

    sync auto-stage code from yyh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMiddle.java
@@ -6,26 +6,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+@Autonomous(name = "Middle_Auto")
 
 
-@Autonomous(name = "Blueside_Left_Auto")
-
-
-public class AutoBluesideLeft extends LinearOpMode {
+public class AutoMiddle extends LinearOpMode {
     Hardwaremap autohwp = new Hardwaremap();
     EncoderDrive eg =new EncoderDrive();
 
 
-    static final double ANDYMARK_PER_MOTOR_REV = 1120;    // eg: ANDYMARK Motor Encoder
+    static final double ANDYMARK_PER_MOTOR_REV = 560;    // eg: ANDYMARK Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES = 3;     // For figuring circumference
+    static final double WHEEL_DIAMETER_INCHES = 4;     // For figuring circumference
     static final double COUNTS_PER_INCH = (ANDYMARK_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
     ElapsedTime runtime = new ElapsedTime();
 
 
-    static final double DRIVE_SPEED = 60;
+    static final double DRIVE_SPEED = 0.4;
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -36,6 +36,16 @@ public class AutoBluesideLeft extends LinearOpMode {
         autohwp.Rightfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         autohwp.Leftback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         autohwp.Rightback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        autohwp.Leftfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        autohwp.Leftback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        autohwp.Rightback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        autohwp.Rightfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        autohwp.Leftfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        autohwp.Leftback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        autohwp.Rightback.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        autohwp.Rightfront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         autohwp.Leftfront.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -82,17 +92,23 @@ public class AutoBluesideLeft extends LinearOpMode {
 
             // reset the timeout time and start motion.
             runtime.reset();
-            autohwp.Leftfront.setVelocity(Math.abs(speed));
-            autohwp.Rightfront.setVelocity(Math.abs(speed));
-            autohwp.Leftback.setVelocity(Math.abs(speed));
-            autohwp.Rightback.setVelocity(Math.abs(speed));
+            autohwp.Leftfront.setPower(-speed);
+            autohwp.Rightfront.setPower(-speed);
+            autohwp.Leftback.setPower(-speed);
+            autohwp.Rightback.setPower(-speed);
             // keep looping while we are still active, and there is time left, and both motors are running.
             // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
             // its target position, the motion will stop.  This is "safer" in the event that the robot will
             // always end the motion as soon as possible.
             // However, if you require that BOTH motors have finished their moves before the robot continues
             // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() && runtime.seconds() < timeoutS && (autohwp.Leftfront.isBusy()&& autohwp.Leftback.isBusy() && autohwp.Rightback.isBusy() && autohwp.Rightfront.isBusy())) {
+            while (opModeIsActive() && runtime.seconds() < timeoutS &&
+                    Math.abs(-autohwp.Leftfront.getCurrentPosition() - autohwp.Leftfront.getTargetPosition()) > 50 &&
+                    Math.abs(-autohwp.Leftback.getCurrentPosition() - autohwp.Leftback.getTargetPosition()) > 50 &&
+                    Math.abs(-autohwp.Rightfront.getCurrentPosition() - autohwp.Rightfront.getTargetPosition()) > 50 &&
+                    Math.abs(-autohwp.Rightback.getCurrentPosition() - autohwp.Rightback.getTargetPosition()) > 50) {
+
+
                 // Display it for the driver.
                 telemetry.addData("Path1", autohwp.Leftfront.getTargetPosition());
                 telemetry.addData("Path2", autohwp.Leftfront.getCurrentPosition());
